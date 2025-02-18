f16bea96d834e367424287f713fd4bb0005e1c53
Leandro Romanzini <leandro.romanzini@gmail.com>
2025-02-18 17:50:39 -0300

    feat(init.lua): enable Nerd Font support
    Enabled Nerd Font support by setting `vim.g.have_nerd_font` to true.
    This change allows the use of Nerd Font icons in the Neovim configuration.

--- init.lua
+++ init.lua
@@ -91,7 +91,9 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+-- Leandro
+-- Enable Nerd Font.
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
