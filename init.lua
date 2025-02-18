a914b5889f602bf09ce05dfca4660495ea6cb3a0
Leandro Romanzini <leandro.romanzini@gmail.com>
2025-02-18 16:52:40 -0300

    feat: add cursor shape and behavior configuration
    Added a new configuration to define the cursor shape and behavior in Neovim.
    The cursor is set to blink with the option 'a:blinkon1'. This change enhances
    the visual feedback for cursor movements.

--- init.lua
+++ init.lua
@@ -171,6 +171,10 @@ do
   -- instead raise a dialog asking if you wish to save the current file(s)
   -- See `:help 'confirm'`
   vim.o.confirm = true
+
+  -- Leandro
+  -- Defines the cursor shape and behavior.
+  vim.opt.guicursor = 'a:blinkon1'
 end
 
 -- ============================================================
