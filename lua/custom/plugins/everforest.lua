72f3ca02d2fe5dadefcf7f2bb758811d3e33602e
Leandro Romanzini <leandro.romanzini@gmail.com>
2025-02-18 17:03:32 -0300

    feat(colorscheme): switch from tokyonight to everforest
    Commented out the default tokyonight colorscheme configuration in init.lua.
    Added a new plugin configuration for everforest colorscheme in
    lua/custom/plugins/everforest.lua. The everforest colorscheme is now loaded
    with high priority and without lazy loading.

---
+++ lua/custom/plugins/everforest.lua
@@ -0,0 +1,8 @@
+-- Everforest colorscheme configuration for vim.pack plugin manager
+
+vim.pack.add { 'https://github.com/sainnhe/everforest' }
+
+-- Optionally configure and load the colorscheme
+-- vim.g.everforest_enable_italic = true
+
+vim.cmd.colorscheme 'everforest'
