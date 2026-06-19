8a80f7cc50af6ed7022baf6112b3005288abd74a
Leandro Romanzini <leandro.romanzini@gmail.com>
2026-06-18 21:56:28 -0300

    fix(plugins): install rest.nvim dependencies and add Lua package path resolvers

--- lua/custom/plugins/rest.lua
+++ lua/custom/plugins/rest.lua
@@ -1,3 +1,19 @@
 -- rest.nvim configuration for vim.pack plugin manager
 
-vim.pack.add { 'https://github.com/rest-nvim/rest.nvim' }
+-- Install rest.nvim and all its required dependencies
+vim.pack.add {
+  'https://github.com/rest-nvim/rest.nvim',
+  'https://github.com/nvim-neotest/nvim-nio',
+  'https://github.com/lunarmodules/lua-mimetypes',
+  'https://github.com/manoelcampos/xml2lua',
+}
+
+-- Add xml2lua and lua-mimetypes root directories to Lua's package search path.
+-- Since they do not follow the standard Neovim folder structure (which uses /lua),
+-- we need to let Lua know where to find their source files directly.
+local opt_dir = vim.fs.joinpath(vim.fn.stdpath('data'), 'site', 'pack', 'core', 'opt')
+local xml2lua_dir = vim.fs.joinpath(opt_dir, 'xml2lua')
+local mimetypes_dir = vim.fs.joinpath(opt_dir, 'lua-mimetypes')
+
+package.path = package.path .. ';' .. xml2lua_dir .. '/?.lua'
+package.path = package.path .. ';' .. mimetypes_dir .. '/?.lua'
